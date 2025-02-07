ac1da59b709e5c38ba13cb19fc23ad711dfef55f
TimGDev <timothy.m.gregory@gmail.com>
2025-02-07 16:22:35 -0500

    first react project complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+function Avatar() {
+  return <img className="avatar" src="tim.jpeg" alt="me" />;
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Timothy Gregory</h1>
+      <p>
+        A senior conslutant at Sogeti with over 10 years of Web/Software
+        development. When not coding/studing for my carrer I love to play video
+        games and make music
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill name="HTML+CSS" emoji="💪" color="red" />
+      <Skill name="React" emoji="👶" color="yellow" />
+      <Skill name="C#" emoji="💪" color="orange" />
+      <Skill name=".Net" emoji="💪" color="purple" />
+      <Skill name="Javascript" emoji="💪" color="red" />
+      <Skill name="Azure" emoji="👶" color="blue" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.name}</span>
+      <span>{props.emoji}</span>
     </div>
   );
 }
